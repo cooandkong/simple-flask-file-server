907e270ba454d0a40d265c41eea52a771a9c7920
kwon tae min <cooandkong@gmail.com>
2023-01-30 16:35:13 +0900

    Add 'file-server-with-flask' upload file list page

--- file-server-with-flask/app.py
+++ file-server-with-flask/app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import render_template
-from flask import request
 from flask_wtf import FlaskForm  # 폼 관리, CSRF보호, 유효성 검사, 리캡차 기능
 from flask_wtf.file import FileField, FileRequired
 from werkzeug.utils import secure_filename  # 사용자 업로드 파일명 검증
@@ -29,13 +28,14 @@ def stamp2real(stamp):
 	return datetime.datetime.fromtimestamp(stamp)
 
 
-# 파일 정보
-def info(filename):
-	ctime = os.path.getctime(filename)
-	mtime = os.path.getmtime(filename)
-	atime = os.path.getatime(filename)
-	size = os.path.getsize(filename)
-	return ctime, mtime, atime, size
+def getctime(file_path):
+	return os.path.getctime(file_path)
+
+def getmtime(file_path):
+	return os.path.getmtime(file_path)
+
+def getsize(file_path):
+	return os.path.getsize(file_path)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -43,21 +43,20 @@ def upload_page():
 	form = FileForm()
 	if form.validate_on_submit():
 		f = form.files.data
-		f.save('./uploads/' + secure_filename(f.filename))
+		f.save('./uploads/' + f.filename)
+		return render_template('check.html', pwd=os.getcwd() + "\\uploads")
 
-		file_info = {}
-		ctime, mtime, atime, size = info('./uploads/' + f.filename)
-		file_info["create"] = stamp2real(ctime)
-		file_info["modify"] = stamp2real(ctime)
-		file_info["access"] = stamp2real(ctime)
-		if size <= 1000000:
-			file_info["size"] = "%.2f KB" % (size / 1024)
-		else:
-			file_info["size"] = "%.2f MB" % (size / (1024.0 * 1024.0))
-
-		return render_template('check.html', file_info=file_info)
-	return render_template('upload.html', form=form)
+	filelist = os.listdir('./uploads')
+	infos = [{
+		"name": name,
+		"extension": os.path.splitext(name)[1].lower(),
+		"create": stamp2real(getctime('./uploads/' + name)),
+		"modify": stamp2real(getmtime('./uploads/' + name)),
+		"size": "%.2f KB" % (getsize('./uploads/' + name) / 1024) if getsize('./uploads/' + name) / 1024 <= 1000000
+		else "%.2f MB" % (getsize('./uploads/' + name) / 1024 <= 1000000 / (1024.0 * 1024.0))
+	} for name in filelist]
+	return render_template('home.html', form=form, pwd=os.getcwd() + "\\uploads", infos=infos)
 
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
